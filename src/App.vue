8e6e2540a5923a5aaf62bf8c9b59df6a46152553
Bestian Tang <bestian@gmail.com>
2024-07-07 04:56:04 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">FAQ</router-link>
+    <router-link to="/faq">FAQ</router-link>
   </nav>
   <router-view/>
 </template>
