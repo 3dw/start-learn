ea9f6e146eb92b17b84f72f3a809cdd2673e0bc6
Bestian Tang <bestian@gmail.com>
2024-07-08 07:09:35 +0800

    資料規格化

---
+++ src/data/catagories.js
@@ -0,0 +1,23 @@
+
+export const catagories = [
+  { "t": "起步", 
+    "description": "自學法源與申請流程", 
+    "color": "#9ef", 
+    "icon": "law"
+  },
+  { "t": "計畫",
+     "description": "做自學計畫時可以想想這些面向", 
+     "color": "#0f0", 
+     "icon": "hourglass half"
+  },    
+  { "t": "支持", 
+    "description": "自學的支持力量來自家庭與朋友", 
+    "color": "#f93", 
+    "icon": "home"
+  },
+  { "t": "資源", 
+    "description": "文化與經濟上的資源",
+    "color": "#c9c",
+    "icon": "in cart",
+  }
+]
