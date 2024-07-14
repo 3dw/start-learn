d21881597f3ba26bfc96fdc5a65f0583bd7d5e5d
Bestian Tang <bestian@gmail.com>
2024-07-15 07:34:16 +0800

    add ad

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,22 @@ nav.ui.menu
   router-link.item(to="/faq") 問答集
   router-link.item(to="/four") 四階段
 router-view
+ad.fat-only
 </template>
+
+<script>
+
+import Ad from './components/Ad-Be.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AutoMap',
+  components: {
+    Ad
+  }
+})
+
+</script>
 
 <style>
 #app {
@@ -17,6 +32,34 @@ router-view
 
 nav a.router-link-exact-active {
   color: #42b983 !important;
+}
+
+
+a, button, .clickable {
+  cursor: pointer !important;
+}
+
+@media screen and (max-width: 600px) {
+  .fat-only {
+    display: none !important;
+  }
+  #ad {
+    position: fixed;
+    bottom: 0;
+    right: 0 !important;
+  }
+}
+
+@media screen and (max-width: 991px) {
+  .fater-only {
+    display: none !important;
+  }
+}
+
+@media screen and (min-width: 601px) {
+  .thin-only {
+    display: none !important;
+  }
 }
 
 </style>
---
+++ src/components/Ad-Be.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+.hello
+  #ad
+    a(href="https://www.alearn.org.tw", target="_blank", rel="noopener noreferrer")
+      img.ad(src="/ad-be.jpg" alt="按此瞭解更多")
+      | 自主學習進促進會製作
+</template>
+
+<script>
+
+export default {
+  name: 'AdComp',
+  data() {
+    return { };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#ad {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(255,255,255,0.8);
+  border-radius: 5px;
+  padding: .5em .5em;
+  font-size: 16px;
+  transition: all 0.5s ease;
+}
+
+#ad:hover {
+  font-size: 20px;
+}
+
+.ad {
+  width: 3em;
+}
+
+</style>
