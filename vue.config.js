f511906d338b5cd38da049682d287672ee0629b6
Bestian Tang <bestian@gmail.com>
2024-07-14 14:11:03 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'StartLearn',
+    themeColor: '#00e600',
+    msTileColor: '#ff99ff',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxOptions: {
+      exclude: [/\.gitkeep/],
+      skipWaiting: true,
+      clientsClaim: true
+   }
+  }
 })
